6eb48b0775595ff559868f43f9b34bcbf4223565
gyuen01 <gyuen01@yahoo.com>
2024-06-05 15:08:31 -0700

    Describe your changes here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import supabase from "../src/config/supabaseClient";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Home = () => {
+    const [fetchError, setFetchError] = useState(null);
+    const [models, setModels] = useState(null);
+    console.log(supabase);
+    
+    useEffect(() => {
+        const fetchModels = async () => {  // Corrected function name and 'async' keyword
+            const { data, error } = await supabase
+                .from('Models')
+                .select();
+
+            if (error) {
+                console.error("Fetch Error:", error);
+                setFetchError('Could not fetch the models');  // Corrected typo in the error message
+                setModels(null);
+                console.log(error);
+            }
+            if (data) {
+                console.log("Data:", data);
+                setModels(data);
+                setFetchError(null);
+            }
+        };
+        
+        fetchModels();
+
+    }, []);
+    //console.log("data", data);
+    return (
+        <div className="page Home">
+            <h2>Home Gary</h2>
+            {fetchError && (<p>{fetchError}</p>)}
+            {models && (
+                <div className="Models">
+                    {models.map(model => (
+                        <div key={model.id}>
+                            <p>Model Name: {model.modelname}</p>
+                            <p>Created Date: {new Date(model.createdDate).toLocaleDateString()}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default App;
+export default Home;
